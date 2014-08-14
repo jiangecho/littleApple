c0752a8c8cce993e1e45f00b72f2ab23239c1e2a
jiang echo <jiangyingji1@163.com>
2014-08-14 17:48:20 +0800

    chagne encode

--- src/com/echo/littleapple/GameActiviy.java
+++ src/com/echo/littleapple/GameActiviy.java
@@ -297,7 +297,7 @@ public class GameActiviy extends Activity implements GameEventListner{
 	public void onShareButtonClick(View view){
 		String imgPath = takeScreenShot(view);
 		if (imgPath == null) {
-			Toast.makeText(this, "SD��������", Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "SD卡不存在", Toast.LENGTH_SHORT).show();
 		}else {
 			showShare(imgPath);
 		}
@@ -334,29 +334,29 @@ public class GameActiviy extends Activity implements GameEventListner{
    private void showShare(String imgPath) {
         ShareSDK.initSDK(this);
         OnekeyShare oks = new OnekeyShare();
-        //�ر�sso��Ȩ
+        //关闭sso授权
         oks.disableSSOWhenAuthorize();
        
-        // ����ʱNotification��ͼ�������
+        // 分享时Notification的图标和文字
         oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-        // title���⣬ӡ��ʼǡ����䡢��Ϣ��΢�š��������QQ�ռ�ʹ��
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
         oks.setTitle(getString(R.string.app_name));
-        // titleUrl�Ǳ�����������ӣ������������QQ�ռ�ʹ��
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
         oks.setTitleUrl(APP_URL);
-        // text�Ƿ����ı�������ƽ̨����Ҫ����ֶ�
-        oks.setText("����������ս�Ұɣ������ҵ�Сƻ��:" + APP_URL);
-        // imagePath��ͼƬ�ı���·����Linked-In�����ƽ̨��֧�ִ˲���
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("哈哈，来挑战我吧！你是我的小苹果:" + APP_URL);
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         oks.setImagePath(imgPath);
-        // url����΢�ţ��������Ѻ�����Ȧ����ʹ��
+        // url仅在微信（包括好友和朋友圈）中使用
         oks.setUrl(APP_URL);
-        // comment���Ҷ�������������ۣ������������QQ�ռ�ʹ��
-        oks.setComment("�Ǻǣ��ҳ���" + gameView.getScore() + "��Сƻ��");
-        // site�Ƿ�������ݵ���վ��ƣ�����QQ�ռ�ʹ��
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("呵呵，我吃了" + gameView.getScore() + "个小苹果！");
+        // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
-        // siteUrl�Ƿ�������ݵ���վ��ַ������QQ�ռ�ʹ��
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
         oks.setSiteUrl(APP_URL);
 
-        // ��������GUI
+        // 启动分享GUI
         oks.show(this);
    }
    
