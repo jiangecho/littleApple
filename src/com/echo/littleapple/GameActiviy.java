2ebc2820ab317c78455edf3c94a74c73c6fedd41
jiang echo <jiangyingji1@163.com>
2014-08-26 20:55:36 +0800

    fix bug: do not asign any random name for users

--- src/com/echo/littleapple/GameActiviy.java
+++ src/com/echo/littleapple/GameActiviy.java
@@ -102,7 +102,7 @@ public class GameActiviy extends Activity implements GameEventListner{
         
         nickyName = sharedPreferences.getString(NICKY_NAME, null);
         if (nickyName == null) {
-        	nickyName = "User" + System.currentTimeMillis();
+        	//nickyName = "User" + System.currentTimeMillis();
         	LayoutInflater layoutInflater = LayoutInflater.from(this);
         	final View view = layoutInflater.inflate(R.layout.nicky_name, null);
         	AlertDialog dialog = new AlertDialog.Builder(this)
