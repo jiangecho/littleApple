3dc9c3ea78555e78b08f48937f5dfce5d73d389c
jiang echo <jiangyingji1@163.com>
2014-09-30 16:54:40 +0800

    Revert "add flappy bird"
    This reverts commit 6f6593e3a3611e29aeaddc843d4c78d4ba238a33.

--- src/com/echo/littleapple/GameActiviy.java
+++ src/com/echo/littleapple/GameActiviy.java
@@ -17,7 +17,6 @@ import java.util.Random;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-import android.R.integer;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -363,11 +362,6 @@ level = LEVEL_HARD;
 		});
 		
 		dialog.show();
-	}
-	
-	public void onMoreGameButtonClick(View view){
-		Intent intent = new Intent(this, com.jucyzhang.flappybatta.GameActivity.class);
-		startActivity(intent);
 	}
 	
 	//select type
